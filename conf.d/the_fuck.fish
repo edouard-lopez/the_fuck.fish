d0fb4cdee2ffdedbf0ada151a3d5f780b9738c6d
Édouard Lopez <contact@edouard-lopez.com>
2025-12-15 12:37:50 +0100

    chore: cleanup on uninstall

--- conf.d/the_fuck.fish
+++ conf.d/the_fuck.fish
@@ -2,3 +2,9 @@ set --global the_fuck_version 0.1.1
 set --universal THE_FUCK_ENABLED 1
 
 functions --query the_fuck # attach event handler
+
+# Clean up variables
+function _the_fuck_uninstall --on-event the_fuck_uninstall
+  set --erase the_fuck_version
+  set --erase --universal THE_FUCK_ENABLED
+end
